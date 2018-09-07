d47f35157bbbd35593390626344a2f684059d77d
JaoK <jjaokk@gmail.com>
2018-09-07 14:51:19 +0200

    genomgång 7/9
    var så goda

---
+++ v36/intro/genomgång.java
@@ -0,0 +1,56 @@
+package intro;
+
+import java.util.Scanner;
+
+public class genomgång {
+
+	public static void main(String[] args) {
+		
+		int tal = 23;
+		double decimal = 123.456;
+		
+		decimal = 50;
+		tal = 50;
+		
+		String Joakim = "hej!";
+		String namn = Joakim;
+		String str = new String("str");
+				
+		char c1 = 'c';
+		char c2 = 'j';
+		
+		boolean b1 = true;
+		boolean b2 = false;	
+		
+		if(b1){
+			System.out.println("b1 är sann");			
+		}
+		else if(b1){
+			
+		}
+		else if(b1){
+			
+		}
+		else{
+			System.out.println("b1 är INTE sann");
+		}
+		
+		if(tal == 50){
+			System.out.println(tal*50);
+		}
+		
+		Scanner input = new Scanner(System.in);
+
+		String rad1 = input.nextLine();
+		String rad2 = input.nextLine();
+		String rad3 = input.nextLine();
+		String rad4 = input.nextLine();
+				
+		System.out.println(rad1 +" konkatenera "+ rad2 + rad3 + rad4);
+		
+		System.out.print("hej\n");
+		System.out.println("hej"); // Lägger på \n automatiskt
+		
+	}
+	
+}
